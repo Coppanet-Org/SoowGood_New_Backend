0caad4607e3b806fe3605220d20b8006c08903fb
ShaikatCopnet <shaikat.b@coppanet.com>
2023-07-28 00:47:06 +0600

    Document Attachement Service Development Complete

---
+++ src/SoowGoodWeb.Application/Services/DocumentsAttachmentService.cs
@@ -0,0 +1,32 @@
+﻿using SoowGoodWeb.Enums;
+using SoowGoodWeb.DtoModels;
+using SoowGoodWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.ObjectMapping;
+
+namespace SoowGoodWeb.Services
+{
+    public class DocumentsAttachmentService : CrudAppService<Attachment, DocumentsAttachmentDto, int>
+    {
+        private readonly IRepository<Attachment, int> repository;
+        public DocumentsAttachmentService(IRepository<Attachment, int> repository) : base(repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<List<DocumentsAttachmentDto>?> GetAttachmentInfo(string entityType, int? entityId, string attachmentType)
+        {
+            var attachment = await repository.GetListAsync(x => x.EntityType == (EntityType)Enum.Parse(typeof(EntityType), entityType) && x.EntityId == entityId && x.AttachmentType == (AttachmentType)Enum.Parse(typeof(AttachmentType), attachmentType));            
+            if (attachment != null)
+            {
+                return ObjectMapper.Map<List<Attachment>, List<DocumentsAttachmentDto>>((List<Attachment>)attachment);
+            }
+
+            return null;
+        }
+    }
+}
--- src/SoowGoodWeb.HttpApi.Host/Controllers/CommonController.cs
+++ src/SoowGoodWeb.HttpApi.Host/Controllers/CommonController.cs
@@ -17,21 +17,18 @@ namespace SoowGoodWeb.Controllers
     [ApiController]
     public class CommonController : AbpController
     {
-        private readonly IWebHostEnvironment webHostEnvironment;
-        private readonly IRepository<Attachment> attachmentRepository;
-        //private readonly IRepository<Complain> complainRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IRepository<Attachment> _attachmentRepository;
         public CommonController(
-            IWebHostEnvironment _webHostEnvironment,
-            IRepository<Attachment> _attachmentRepository
-            //IRepository<Complain> _complainRepository
+            IWebHostEnvironment webHostEnvironment,
+            IRepository<Attachment> attachmentRepository
            )
         {
-            webHostEnvironment = _webHostEnvironment;
-            attachmentRepository = _attachmentRepository;
-            //complainRepository = _complainRepository;
+            _webHostEnvironment = webHostEnvironment;
+            _attachmentRepository = attachmentRepository;
         }
 
-        [HttpPost, ActionName("Complain")]
+        [HttpPost, ActionName("UploadDocuments")]
         [DisableRequestSizeLimit]
         public IActionResult FileUploadComplain()
         {
@@ -41,22 +38,15 @@ namespace SoowGoodWeb.Controllers
                 var files = Request.Form.Files;
                 if (files.Count > 0)
                 {
-                    var complainIdStr = Request.Form["complaintId"].ToString();
-                    int complainId = string.IsNullOrEmpty(complainIdStr) ? 0 : Convert.ToInt32(complainIdStr);
+                    var idStr = Request.Form["entityId"].ToString();
+                    int id = string.IsNullOrEmpty(idStr) ? 0 : Convert.ToInt32(idStr);
                     var entityType = Request.Form["entityType"].ToString();
-                    if (string.IsNullOrEmpty(entityType))
-                    {
-                        entityType = "None";
-                    }
-                    var attachmentType = Request.Form["attachmentType"].ToString();
-                    if (string.IsNullOrEmpty(attachmentType))
-                    {
-                        attachmentType = "None";
-                    }
-                    var directoryName = Request.Form["directoryName"][0];
-                    var folderName = Path.Combine("wwwroot", "uploads", directoryName);
-                    int insertCount = 0;
 
+                    var attachmentType = Request.Form["attachmentType"].ToString();
+
+                    var directoryName = Request.Form["directoryName"][0];
+                    var folderName = Path.Combine("wwwroot", "uploads", !string.IsNullOrEmpty(directoryName) ? directoryName : "Misc");
+                    int insertCount = 0;
                     foreach (var file in files)
                     {
                         if (!Directory.Exists(folderName))
@@ -78,9 +68,9 @@ namespace SoowGoodWeb.Controllers
                         attachement.OriginalFileName = fileName;
                         attachement.Path = dbPath;
                         attachement.EntityType = (EntityType)Enum.Parse(typeof(EntityType), entityType);
-                        attachement.EntityId = complainId;
+                        attachement.EntityId = id;
                         attachement.AttachmentType = (AttachmentType)Enum.Parse(typeof(AttachmentType), attachmentType);
-                        attachmentRepository.InsertAsync(attachement);
+                        _attachmentRepository.InsertAsync(attachement);
                         insertCount += 1;
                         dbPath = dbPath.Replace(@"wwwroot\", string.Empty);
                     }
@@ -113,160 +103,129 @@ namespace SoowGoodWeb.Controllers
             }
         }
 
-        [HttpPost, ActionName("Allotment")]
-        [DisableRequestSizeLimit]
-        public IActionResult FileUploadAllotment()
+
+
+        [HttpPost, ActionName("DeleteFileComplain")]
+        public IActionResult DeleteFileComplain(FileDeleteInputDto input)
         {
-            Dictionary<string, string> result = new Dictionary<string, string>();
             try
             {
-                var files = Request.Form.Files;
-                if (files.Count > 0)
+                var filePath = Path.Combine(_webHostEnvironment.WebRootPath, input.FilePath);
+                FileInfo fi = new FileInfo(filePath);
+                if (fi != null)
                 {
-                    var allotmentIdStr = Request.Form["allotmentId"].ToString();
-                    int allotmentId = string.IsNullOrEmpty(allotmentIdStr) ? 0 : Convert.ToInt32(allotmentIdStr);
-                    var entityType = Request.Form["entityType"].ToString();
-                    if (string.IsNullOrEmpty(entityType))
-                    {
-                        entityType = "None";
-                    }
-                    var attachmentType = Request.Form["attachmentType"].ToString();
-                    if (string.IsNullOrEmpty(attachmentType))
-                    {
-                        attachmentType = "None";
-                    }
-                    var directoryName = Request.Form["directoryName"][0];
-                    var folderName = Path.Combine("wwwroot", "uploads", directoryName);
-                    int insertCount = 0;
-
-                    foreach (var file in files)
-                    {
-                        if (!Directory.Exists(folderName))
-                        {
-                            DirectoryInfo di = Directory.CreateDirectory(folderName);
-                        }
-
-                        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
-                        var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                        var fullPath = Path.Combine(pathToSave, fileName);
-                        var dbPath = Path.Combine(folderName, fileName);
-                        using (var stream = new FileStream(fullPath, FileMode.Create))
-                        {
-                            file.CopyTo(stream);
-                        }
-                        // save attachment
-                        var attachement = new Attachment();
-                        attachement.FileName = fileName;
-                        attachement.OriginalFileName = fileName;
-                        attachement.Path = dbPath;
-                        attachement.EntityType = (EntityType)Enum.Parse(typeof(EntityType), entityType);
-                        attachement.EntityId = allotmentId;
-                        attachement.AttachmentType = (AttachmentType)Enum.Parse(typeof(AttachmentType), attachmentType);
-                        attachmentRepository.InsertAsync(attachement);
-                        insertCount += 1;
-                        dbPath = dbPath.Replace(@"wwwroot\", string.Empty);
-                    }
-
-                    if (insertCount > 0)
-                    {
-                        result.Add("Status", "Success");
-                        result.Add("Message", "Data save successfully!");
-                    }
-                    else
-                    {
-                        result.Add("Status", "Warning");
-                        result.Add("Message", "Fail to save!");
-                    }
-
-                    return new JsonResult(result);
+                    System.IO.File.Delete(filePath);
+                    fi.Delete();
                 }
-                else
-                {
-                    result.Add("Status", "Warning");
-                    result.Add("Message", "Attachment not found!");
-                    return new JsonResult(result);
-                }
+                return new JsonResult(input.FilePath);
             }
             catch (Exception ex)
             {
-                result.Add("Status", "Error");
-                result.Add("Message", $"Internal server error: {ex}");
-                return new JsonResult(result);
+                return StatusCode(500, $"Internal server error: {ex}");
             }
         }
 
-        //[HttpPost, ActionName("DeleteFileComplain")]
-        //public IActionResult DeleteFileComplain(FileDeleteInput input)
+        [HttpPost, ActionName("DeleteFileAllotment")]
+        public IActionResult DeleteFileAllotment(FileDeleteInputDto input)
+        {
+            try
+            {
+                var filePath = Path.Combine(_webHostEnvironment.WebRootPath, input.FilePath);
+                FileInfo fi = new FileInfo(filePath);
+                if (fi != null)
+                {
+                    System.IO.File.Delete(filePath);
+                    fi.Delete();
+                }
+                return new JsonResult(input.FilePath);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+
+        //[HttpPost, ActionName("Allotment")]
+        //[DisableRequestSizeLimit]
+        //public IActionResult FileUploadAllotment()
         //{
+        //    Dictionary<string, string> result = new Dictionary<string, string>();
         //    try
         //    {
-        //        var filePath = Path.Combine(webHostEnvironment.WebRootPath, input.FilePath);
-        //        FileInfo fi = new FileInfo(filePath);
-        //        if (fi != null)
+        //        var files = Request.Form.Files;
+        //        if (files.Count > 0)
         //        {
-        //            System.IO.File.Delete(filePath);
-        //            fi.Delete();
-        //        }
-        //        return new JsonResult(input.FilePath);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, $"Internal server error: {ex}");
-        //    }
-        //}
-
-        //[HttpPost, ActionName("DeleteFileAllotment")]
-        //public IActionResult DeleteFileAllotment(FileDeleteInput input)
-        //{
-        //    try
-        //    {
-        //        var filePath = Path.Combine(webHostEnvironment.WebRootPath, input.FilePath);
-        //        FileInfo fi = new FileInfo(filePath);
-        //        if (fi != null)
-        //        {
-        //            System.IO.File.Delete(filePath);
-        //            fi.Delete();
-        //        }
-        //        return new JsonResult(input.FilePath);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, $"Internal server error: {ex}");
-        //    }
-        //}
-
-        //[HttpPost, ActionName("Example")]
-        //public async void Example2()
-        //{
-        //    DateTime day = (DateTime.Now).Date;
-        //    var sdePhone = "";
-        //    var complaints = await complainRepository.WithDetailsAsync(p => p.ComplainHistories);
-        //    complaints = complaints.Where(c => c.IsDeleted == false
-        //                                    && c.ComplainStatus == ComplainStatus.New);
-        //    if (complaints.Count() > 0)
-        //    {
-        //        foreach (var c in complaints)
-        //        {
-        //            var dc = (day - c.Date.Date).TotalDays;
-        //            if (dc >= 3)
+        //            var allotmentIdStr = Request.Form["allotmentId"].ToString();
+        //            int allotmentId = string.IsNullOrEmpty(allotmentIdStr) ? 0 : Convert.ToInt32(allotmentIdStr);
+        //            var entityType = Request.Form["entityType"].ToString();
+        //            if (string.IsNullOrEmpty(entityType))
         //            {
-        //                //sdePhone = organizaitonUnitService.GetPostingById(c.PostingId).Result.EmpPhoneMobile;
-        //                if (!string.IsNullOrEmpty(sdePhone))
-        //                {
-        //                    SmsRequestInput otpInput2 = new SmsRequestInput();
-        //                    otpInput2.Sms = String.Format("adfadfadf");
-        //                    otpInput2.CsmsId = sdePhone;
-        //                    otpInput2.Msisdn = "";
+        //                entityType = "None";
+        //            }
+        //            var attachmentType = Request.Form["attachmentType"].ToString();
+        //            if (string.IsNullOrEmpty(attachmentType))
+        //            {
+        //                attachmentType = "None";
+        //            }
+        //            var directoryName = Request.Form["directoryName"][0];
+        //            var folderName = Path.Combine("wwwroot", "uploads", directoryName);
+        //            int insertCount = 0;
 
-        //                    //var res2 = await notificationAppService.SendSmsTestAlpha(otpInput2);
+        //            foreach (var file in files)
+        //            {
+        //                if (!Directory.Exists(folderName))
+        //                {
+        //                    DirectoryInfo di = Directory.CreateDirectory(folderName);
         //                }
 
+        //                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+        //                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+        //                var fullPath = Path.Combine(pathToSave, fileName);
+        //                var dbPath = Path.Combine(folderName, fileName);
+        //                using (var stream = new FileStream(fullPath, FileMode.Create))
+        //                {
+        //                    file.CopyTo(stream);
+        //                }
+        //                // save attachment
+        //                var attachement = new Attachment();
+        //                attachement.FileName = fileName;
+        //                attachement.OriginalFileName = fileName;
+        //                attachement.Path = dbPath;
+        //                attachement.EntityType = (EntityType)Enum.Parse(typeof(EntityType), entityType);
+        //                attachement.EntityId = allotmentId;
+        //                attachement.AttachmentType = (AttachmentType)Enum.Parse(typeof(AttachmentType), attachmentType);
+        //                attachmentRepository.InsertAsync(attachement);
+        //                insertCount += 1;
+        //                dbPath = dbPath.Replace(@"wwwroot\", string.Empty);
         //            }
+
+        //            if (insertCount > 0)
+        //            {
+        //                result.Add("Status", "Success");
+        //                result.Add("Message", "Data save successfully!");
+        //            }
+        //            else
+        //            {
+        //                result.Add("Status", "Warning");
+        //                result.Add("Message", "Fail to save!");
+        //            }
+
+        //            return new JsonResult(result);
+        //        }
+        //        else
+        //        {
+        //            result.Add("Status", "Warning");
+        //            result.Add("Message", "Attachment not found!");
+        //            return new JsonResult(result);
         //        }
         //    }
-
-        //    //return complaints.ToList().Count;
+        //    catch (Exception ex)
+        //    {
+        //        result.Add("Status", "Error");
+        //        result.Add("Message", $"Internal server error: {ex}");
+        //        return new JsonResult(result);
+        //    }
         //}
-
     }
 }
