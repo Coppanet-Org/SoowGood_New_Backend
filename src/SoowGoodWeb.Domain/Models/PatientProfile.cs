f8da2c555f324820930a7d40ba227e2b799bf740
ShaikatCopnet <shaikat.b@coppanet.com>
2023-08-31 17:23:51 +0600

    ...

---
+++ src/SoowGoodWeb.Domain/Models/PatientProfile.cs
@@ -0,0 +1,33 @@
+﻿using SoowGoodWeb.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace SoowGoodWeb.Models
+{
+    public class PatientProfile : FullAuditedEntity<long>
+    {        
+        public string? FullName { get; set; }
+        public bool? IsSelf { get; set; }
+        public string? PatientName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int? Age { get; set; }
+        public Gender? Gender { get; set; }
+        public string? BloodGroup { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? ZipCode { get; set; }
+        public string? Country { get; set; }
+        public string? MobileNo { get; set; }
+        public string? Email { get; set; }
+        public string? AgentCode { get; set; }
+        public Guid? UserId { get; set; }
+        public bool? IsOnline { get; set; }
+        public int? profileStep { get; set; }
+        public string? createFrom { get; set; }
+
+    }
+}
