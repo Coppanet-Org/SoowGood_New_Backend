0bbf5fe30e46a64fb99cc573fc634380c81f1263
ShaikatCopnet <shaikat.b@coppanet.com>
2023-10-10 12:42:15 +0600

    Prescription Drug Dtos updated

--- src/SoowGoodWeb.Application.Contracts/DtoModels/PrescriptionDrugDetailsDto.cs
+++ src/SoowGoodWeb.Application.Contracts/DtoModels/PrescriptionDrugDetailsDto.cs
@@ -12,7 +12,9 @@ namespace SoowGoodWeb.DtoModels
         public string? PrescriptionRefferenceCode { get; set; }
         public long? DrugRxId { get; set; }
         public string? DrugName { get; set; }
-        public string? Dose { get; set; }
+        public string? DrugDoseSchedule { get; set; }
+        public bool? IsDrugExceptional { get; set; }
+        public string? DrugDoseScheduleDays { get; set; } // if IsDrugExceptional is true
         public string? Duration { get; set; }
         public string? Instruction { get; set; }
     }
--- src/SoowGoodWeb.Application.Contracts/InputDto/PrescriptionDrugDetailsInputDto.cs
+++ src/SoowGoodWeb.Application.Contracts/InputDto/PrescriptionDrugDetailsInputDto.cs
@@ -11,7 +11,9 @@ namespace SoowGoodWeb.InputDto
         public long? PrescriptionMasterId { get; set; }
         public long? DrugRxId { get; set; }
         public string? DrugName { get; set; }
-        public string? Dose { get; set; }
+        public string? DrugDoseSchedule { get; set; }
+        public bool? IsDrugExceptional { get; set; }
+        public string? DrugDoseScheduleDays { get; set; } // if IsDrugExceptional is true
         public string? Duration { get; set; }
         public string? Instruction { get; set; }
 
