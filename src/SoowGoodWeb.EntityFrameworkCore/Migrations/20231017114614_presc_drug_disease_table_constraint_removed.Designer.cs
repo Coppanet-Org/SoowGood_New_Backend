0058f07b7bc3e0c1769e88bdbabd8ba621a76efb
ShaikatCopnet <shaikat.b@coppanet.com>
2023-10-17 18:21:08 +0600

    Bug fixes for prescription generation.

--- src/SoowGoodWeb.EntityFrameworkCore/Migrations/20231017114614_presc_drug_disease_table_constraint_removed.Designer.cs
+++ src/SoowGoodWeb.EntityFrameworkCore/Migrations/20231017114614_presc_drug_disease_table_constraint_removed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoowGoodWeb.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace SoowGoodWeb.Migrations
 {
     [DbContext(typeof(SoowGoodWebDbContext))]
-    partial class SoowGoodWebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017114614_presc_drug_disease_table_constraint_removed")]
+    partial class presc_drug_disease_table_constraint_removed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
