f70ac3052efabcdb6e9a7fd9a95748ff5c6df635
ShaikatCopnet <shaikat.b@coppanet.com>
2023-10-25 04:43:18 -0700

    ....

--- src/SoowGoodWeb.Application/Permissions/PermissionHelper.cs
+++ src/SoowGoodWeb.Application/Permissions/PermissionHelper.cs
@@ -1,11 +1,26 @@
-﻿namespace SoowGoodWeb
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SoowGoodWeb
 {
     public static class PermissionHelper
     {
-        //public static readonly string _authority = "https://198.38.92.117";
-        //public static readonly string _identityClientUrl = "https://198.38.92.117:8443";
-        public static readonly string _authority = "https://localhost:44380";
-        public static readonly string _identityClientUrl = "https://localhost:44392";// http://idapi.mis1pwd.com";
+        //public readonly IConfiguration _appConfiguration;
+        //public PermissionHelper(IConfiguration configurationAccessor)
+        //{
+        //    _appConfiguration = configurationAccessor;
+        //}
+        ////public string SubmitUrl => _appConfiguration["Payment:SslCommerz:SubmitUrl"];
+        //public string _authority => _appConfiguration["AuthServer:Authority"];
+        //public string _identityClientUrl => _appConfiguration["AuthApi:AuthClientUrl"];
+
+        //public string _clientId => _appConfiguration["ClientCredentials:ClientId"]; 
+        //public string _clientSecret => _appConfiguration["ClientCredentials:ClientSecret"];
+        //public string _scope => _appConfiguration["ClientCredentials:Scope"];
+
+        public static readonly string _authority = "https://198.38.92.117";
+        public static readonly string _identityClientUrl = "https://198.38.92.117:8443";
+        //public static readonly string _authority = "https://localhost:44380";
+        //public static readonly string _identityClientUrl = "https://localhost:44392";// http://idapi.mis1pwd.com";
         //public static readonly string _identityClientUrlDev = "https://localhost:44392";
         //public static readonly string _selfClientUrlDev = "https://localhost:44373";
         //public static readonly string _identityApiName = "/api/app/permission-map";
