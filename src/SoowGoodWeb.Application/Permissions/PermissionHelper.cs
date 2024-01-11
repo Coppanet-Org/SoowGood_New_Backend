962d3eede497a81d462a24e5c30fd71f8c0f9c20
ShaikatCopnet <shaikat.b@coppanet.com>
2024-01-11 10:39:01 +0600

    ....

--- src/SoowGoodWeb.Application/Permissions/PermissionHelper.cs
+++ src/SoowGoodWeb.Application/Permissions/PermissionHelper.cs
@@ -21,12 +21,9 @@ namespace SoowGoodWeb
         //public static readonly string _identityClientUrl = "https://103.125.253.240:8443";
 
 
-        public static readonly string _authority = "https://authsoowgoodbeta.com";
-        public static readonly string _identityClientUrl = "https://idapisoowgoodbeta.com";
 
-
-        //public static readonly string _authority = "https://192.168.1.105";
-        //public static readonly string _identityClientUrl = "https://192.168.1.105:8443";
+        public static readonly string _authority = "https://192.168.1.105";
+        public static readonly string _identityClientUrl = "https://192.168.1.105:8443";
 
 
         //public static readonly string _authority = "https://localhost:44380";
