426c51678a8243a2e258836ca22ca653ba6a40ba
ShaikatCopnet <shaikat.b@coppanet.com>
2024-02-07 18:41:16 +0600

    ...

--- src/SoowGoodWeb.Application/Permissions/PermissionHelper.cs
+++ src/SoowGoodWeb.Application/Permissions/PermissionHelper.cs
@@ -4,12 +4,12 @@ namespace SoowGoodWeb
 {
     public static class PermissionHelper
     {
-        //public static readonly string _authority = "https://localhost:44380";
-        //public static readonly string _identityClientUrl = "https://localhost:44392";
+        public static readonly string _authority = "https://localhost:44380";
+        public static readonly string _identityClientUrl = "https://localhost:44392";
 
 
-        public static readonly string _authority = "https://authsoowgoodbeta.com";
-        public static readonly string _identityClientUrl = "https://idapisoowgoodbeta.com";
+        //public static readonly string _authority = "https://authsoowgoodbeta.com";
+        //public static readonly string _identityClientUrl = "https://idapisoowgoodbeta.com";
 
         public static readonly string _clientId = "SoowGoodWeb_App";
         public static readonly string _clientSecret = "1q2w3e*";
