4cf7e9b31198a4766f03d58f4bcc84bbca1c1421
ShaikatCopnet <shaikat.b@coppanet.com>
2024-02-18 18:32:59 +0600

    ......

---
+++ src/SoowGoodWeb.Domain/Models/DiagonsticPackage.cs
@@ -0,0 +1,22 @@
+﻿using SoowGoodWeb.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace SoowGoodWeb.Models
+{
+    public class DiagonsticPackage : FullAuditedEntity<long>
+    {
+        public long? ServiceProviderId { get; set; }
+        public ServiceProvider? ServiceProvider  { get; set; }
+        public long? PathologyCategoryId { get; set; }
+        public PathologyCategory? PathologyCategory { get; set; }
+        public long? PathologyTestId { get; set; }
+        public PathologyTest? PathologyTest { get; set; }
+        public string? PackageName { get; set; }
+        public string? PackageDescription { get; set; }
+    }
+}
