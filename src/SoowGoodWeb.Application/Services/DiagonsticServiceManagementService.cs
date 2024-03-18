2c35e21fce790f650a1390989a54bfc94305d7ec
CoppanetUpama <upama.c@coppanet.com>
2024-03-18 15:48:44 +0600

    ...

--- src/SoowGoodWeb.Application/Services/DiagonsticServiceManagementService.cs
+++ src/SoowGoodWeb.Application/Services/DiagonsticServiceManagementService.cs
@@ -1,4 +1,6 @@
-﻿using SoowGoodWeb.DtoModels;
+﻿using AutoMapper;
+using SoowGoodWeb.DtoModels;
+using SoowGoodWeb.Enums;
 using SoowGoodWeb.InputDto;
 using SoowGoodWeb.Interfaces;
 using SoowGoodWeb.Models;
@@ -61,8 +63,44 @@ namespace SoowGoodWeb.Services
         }
         public async Task<List<DiagonsticPathologyServiceManagementDto>> GetListAsync()
         {
-            var diagonsticPathologyServiceManagements = await _diagonsticPathologyServiceManagementRepository.GetListAsync();
-            return ObjectMapper.Map<List<DiagonsticPathologyServiceManagement>, List<DiagonsticPathologyServiceManagementDto>>(diagonsticPathologyServiceManagements);
+            List<DiagonsticPathologyServiceManagementDto>? result = null;
+
+            var allDiagonsticPathologyServiceRequestDetails = await _diagonsticPathologyServiceManagementRepository.WithDetailsAsync(dp=>dp.DiagonsticPackage,p=>p.ServiceProvider);
+            if (!allDiagonsticPathologyServiceRequestDetails.Any())
+            {
+                return result;
+            }
+            result = new List<DiagonsticPathologyServiceManagementDto>();
+            foreach (var item in allDiagonsticPathologyServiceRequestDetails)
+            {
+
+                result.Add(new DiagonsticPathologyServiceManagementDto()
+                {
+                    Id = item.Id,
+                    DiagonsticPackageId=item.DiagonsticPackageId,
+                    DiagonsticPackageName=item.DiagonsticPackageId>0?item.DiagonsticPackage.PackageName: "n/a",
+                    DiagonsticServiceType = item.DiagonsticServiceType,
+                    DiagonsticServiceTypeName = item.DiagonsticServiceType > 0 ? ((DiagonsticServiceType)item.DiagonsticServiceType).ToString() : "n/a",
+                    Discount = item.Discount,
+                    FinalFee = item.FinalFee,
+                    OrganizationCode = item.OrganizationCode,
+                    PatientCode = item.PatientCode,
+                    PatientName = item.PatientName,
+                    PatientProfileId = item.PatientProfileId,
+                    ProviderFee = item.ProviderFee,
+                    RequestDate = item.RequestDate,
+                    ServiceProviderId = item.ServiceProviderId,
+                    ServiceProviderName=item.ServiceProviderId>0?item.ServiceProvider.ProviderOrganizationName: "n/a",
+                    ServiceRequestCode = item.ServiceRequestCode,
+                    ServiceRequestStatus = item.ServiceRequestStatus,
+                    ServiceRequestStatusName=item.ServiceRequestStatus>0? ((ServiceRequestStatus)item.ServiceRequestStatus).ToString() : "n/a",
+
+                });
+            }
+            var resList = result.OrderByDescending(d => d.Id).ToList();
+            return resList;
+            //var diagonsticPathologyServiceManagements = await _diagonsticPathologyServiceManagementRepository.GetListAsync();
+            //return ObjectMapper.Map<List<DiagonsticPathologyServiceManagement>, List<DiagonsticPathologyServiceManagementDto>>(diagonsticPathologyServiceManagements);
         }   
     }
 }
