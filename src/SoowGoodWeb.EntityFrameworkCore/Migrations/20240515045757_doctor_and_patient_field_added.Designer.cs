5758b33dc55dd75f1eaa20acac26d8d261282425
CoppanetUpama <upama.c@coppanet.com>
2024-05-15 17:40:24 +0600

    ...

--- src/SoowGoodWeb.EntityFrameworkCore/Migrations/20240515045757_doctor_and_patient_field_added.Designer.cs
+++ src/SoowGoodWeb.EntityFrameworkCore/Migrations/20240515045757_doctor_and_patient_field_added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoowGoodWeb.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace SoowGoodWeb.Migrations
 {
     [DbContext(typeof(SoowGoodWebDbContext))]
-    partial class SoowGoodWebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240515045757_doctor_and_patient_field_added")]
+    partial class doctor_and_patient_field_added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
