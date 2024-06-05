3bc05a50e5486ff15aa49d7ded23b93a3890baab
ShaikatCopnet <shaikat.b@coppanet.com>
2024-06-05 12:52:24 +0600

    ...

---
+++ src/SoowGoodWeb.EntityFrameworkCore/SpRepository/DegreeSpRepository.cs
@@ -0,0 +1,27 @@
+﻿using SoowGoodWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Uow;
+
+namespace SoowGoodWeb.SpRepository
+{
+    public class DegreeSpRepository
+    {
+        private readonly IRepository<Degree> _degreeRepository;
+        public DegreeSpRepository(IRepository<Degree> degreeRepository, IUnitOfWorkManager unitOfWorkManager)
+        {
+            _degreeRepository = degreeRepository;
+        }
+        //public List<Degree> GetDegreeList()
+        //{
+        //    List<SqlParams> _params = new List<SqlParams>();
+        //    _params.Add(new SqlParam("@providerid", SqlDbType.NVarChar, providerid));
+        //    _params.Add(new SqlParam("@appointmenttype", SqlDbType.NVarChar, appointmenttype));
+        //    var items = Executor.ExecuteStoredProcedure<ScheduleAppointment>("pr_booking_getclinicappointmentslot", _params);
+        //    return items;
+        //}
+    }
+}
---
+++ src/SoowGoodWeb.EntityFrameworkCore/StoredProcedureManager/SqlParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoowGoodWeb.StoredProcedureManager
+{
+    public class SqlParams
+    {
+        public string ParamName { get; set; }
+        public SqlDbType SqlType { get; set; }
+        public object Value { get; set; }
+        public SqlParams(string paramName, SqlDbType oracleType, object value)
+        {
+            ParamName = paramName;
+            SqlType = oracleType;
+            Value = value;
+        }
+    }
+}
