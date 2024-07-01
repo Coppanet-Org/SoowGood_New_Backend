1d1e1ace36416174c3b4ea4cd74e909bc7636483
CoppanetUpama <upama.c@coppanet.com>
2024-07-01 17:05:03 +0600

    working on Master Doctor

---
+++ src/SoowGoodWeb.Application.Contracts/DtoModels/MasterDoctorDto.cs
@@ -0,0 +1,19 @@
+﻿using SoowGoodWeb.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace SoowGoodWeb.DtoModels
+{
+    public class MasterDoctorDto : FullAuditedEntityDto<long>
+    {
+        public long? DoctorProfileId { get; set; }
+        //public DoctorProfile? DoctorProfile { get; set; }
+        public long? AgentMasterId { get; set; }
+        //public AgentMaster? AgentMaster { get; set; }
+
+        public string? DoctorName { get; set; }
+
+    }
+}
