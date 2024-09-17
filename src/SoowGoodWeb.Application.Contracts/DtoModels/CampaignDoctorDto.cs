9d93a1ba13012a200908a1ffef2ec12f4a0c93fd
CoppanetUpama <upama.c@coppanet.com>
2024-09-17 17:44:57 +0600

    ....

--- src/SoowGoodWeb.Application.Contracts/DtoModels/CampaignDoctorDto.cs
+++ src/SoowGoodWeb.Application.Contracts/DtoModels/CampaignDoctorDto.cs
@@ -12,5 +12,24 @@ namespace SoowGoodWeb.DtoModels
         //public DoctorProfile? DoctorProfile { get; set; }
         public long? CampaignId { get; set; }
         //public Campaign? Campaign { get; set; }
+
+        public string? DoctorName { get; set; }
+        public DoctorTitle? DoctorTitle { get; set; }
+        public string? DoctorTitleName { get; set; }
+        public bool? IsActive { get; set; }
+        public bool? IsOnline { get; set; }
+
+        public List<DoctorDegreeDto>? DoctorDegrees { get; set; }
+        public string? Qualifications { get; set; }
+        public List<DoctorSpecializationDto>? DoctorSpecialization { get; set; }
+        public string? AreaOfExperties { get; set; }
+
+        public string? ProfilePic { get; set; }
+
+        public decimal? DisplayInstantFeeAsAgent { get; set; }
+
+        public decimal? DoctorFee { get; set; }
+
+        public string? DoctorCode { get; set; }
     }
 }
